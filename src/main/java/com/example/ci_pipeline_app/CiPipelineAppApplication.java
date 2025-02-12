0fb145acbc19b6ca474e8a5d463377b1a5392438
Riyadh Ahmed <rahmed70@my.centennialcollege.ca>
2025-02-12 14:51:38 -0500

    "added greeting message"

---
+++ src/main/java/com/example/ci_pipeline_app/CiPipelineAppApplication.java
@@ -0,0 +1,23 @@
+package com.example.ci_pipeline_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CiPipelineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiPipelineAppApplication.class, args);
+		// Inside a controller or service method
+		
+		}
+		@GetMapping
+		public String greeting() {
+			return "Welcome to COMP367";
+
+	}
+
+}
---
+++ src/test/java/com/example/ci_pipeline_app/CiPipelineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ci_pipeline_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiPipelineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
